98a0847938697df64cf115ce1f2d3866fd030283
Kris <107553288+KR1SSL4VCH3V@users.noreply.github.com>
2023-01-20 11:28:59 +0000

    Delete todo_list directory

--- todo_list/todo_list/accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from todo_list.accounts.models import Profile
-
-
-@admin.register(Profile)
-class AdminProfile(admin.ModelAdmin):
-    pass
--- todo_list/todo_list/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('todo_list.tasks.urls')),
-    path('accounts/', include('todo_list.accounts.urls')),
-]
