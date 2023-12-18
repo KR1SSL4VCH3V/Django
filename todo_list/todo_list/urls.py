d5d088e79ef3458af5435b404f3c8dca3253515e
Kris <107553288+KR1SSL4VCH3V@users.noreply.github.com>
2023-12-18 22:34:55 +0000

    Update urls.py

--- todo_list/todo_list/urls.py
+++ todo_list/todo_list/urls.py
@@ -1,9 +1,10 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('todo_list.tasks.urls')),
-    path('accounts/', include('todo_list.accounts.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('todo_list.tasks.urls')),
+    path('api/accounts/', include('todo_list.accounts.urls')),
 ]
