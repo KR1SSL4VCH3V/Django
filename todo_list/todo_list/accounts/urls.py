1bb0f1ff6b86370f81610ff0447c3b47ff1ca5de
Kris <107553288+KR1SSL4VCH3V@users.noreply.github.com>
2023-12-18 22:37:58 +0000

    Update urls.py

--- todo_list/todo_list/accounts/urls.py
+++ todo_list/todo_list/accounts/urls.py
@@ -1,14 +1,12 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
-from todo_list.accounts.views import SignUpView, SignInView, SignOutView, DeleteAccount, EditAccountUser
+from todo_list.accounts.views import SignUpView, SignInView, SignOutView, EditAccountView, DeleteAccount
 
 urlpatterns = [
     path('register/', SignUpView.as_view(), name='register'),
     path('sign-in/', SignInView.as_view(), name='sign-in'),
     path('sign-out/', SignOutView.as_view(), name='sign-out'),
-    path('edit/<int:pk>/', EditAccountUser.as_view(), name='edit'),
+    path('edit/<int:pk>/', EditAccountView.as_view(), name='edit'),
     path('delete/<int:pk>/', DeleteAccount.as_view(), name='delete'),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
