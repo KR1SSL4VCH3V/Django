353794fcc3f94521c5468daa311bac96f283bbb6
Kris <107553288+KR1SSL4VCH3V@users.noreply.github.com>
2023-12-18 22:38:25 +0000

    Update models.py

--- todo_list/todo_list/accounts/models.py
+++ todo_list/todo_list/accounts/models.py
@@ -1,34 +1,43 @@
-
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 
+class CustomUser(AbstractUser):
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        null=False,
+        blank=False,
+    )
+
+    email = models.EmailField(
+        unique=True,
+        null=False,
+        blank=False,
+    )
+
+    class Meta:
+        app_label = 'accounts'
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_users',
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_users',
+    )
+
+
 class Profile(models.Model):
-    username = models.OneToOneField(
-        User,
+    user = models.OneToOneField(
+        CustomUser,
         on_delete=models.CASCADE,
-        null=False,
-        blank=False,
-    )
-    first_name = models.CharField(
-        max_length=39,
-        null=False,
-        blank=False,
-        default=True,
-    )
-
-    last_name = models.CharField(
-        max_length=39,
-        null=False,
-        blank=False,
-        default=True,
-    )
-
-    is_staff = models.BooleanField(
-        default=False,
     )
 
     def __str__(self):
-        return str(self.username)
+        return str(self.user)
 
-
+    def full_clean(self, exclude=None, validate_unique=True, validate_constraints=True):
+        return super().full_clean()
