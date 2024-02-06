acfe675adbe950bb03ada59d9e270d307208e0d6
Kris <107553288+KR1SSL4VCH3V@users.noreply.github.com>
2024-02-06 16:36:25 +0000

    Update models.py

--- todo_list/todo_list/accounts/models.py
+++ todo_list/todo_list/accounts/models.py
@@ -1,43 +1,37 @@
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import AbstractUser, Group, Permission, UserManager
 from django.db import models
+from django.db.models import Q
+
+# Adding CustomUserManager to enable the user to register with a username or email.
+
+class CustomUserManager(UserManager):
+    def get_by_natural_key(self, username):
+        return self.get(
+            Q(**{self.model.USERNAME_FIELD: username}) |
+            Q(**{self.model.EMAIL_FIELD: username})
+        )
 
 
 class CustomUser(AbstractUser):
-    username = models.CharField(
-        max_length=150,
-        unique=True,
-        null=False,
-        blank=False,
-    )
-
-    email = models.EmailField(
-        unique=True,
-        null=False,
-        blank=False,
-    )
-
-    class Meta:
-        app_label = 'accounts'
-
     groups = models.ManyToManyField(
         Group,
-        related_name='custom_users',
+        related_name='custom_users_groups',
     )
 
     user_permissions = models.ManyToManyField(
         Permission,
-        related_name='custom_users',
+        related_name='custom_users_permissions',
     )
 
+    #  Make the email field unique to avoid duplication.
 
-class Profile(models.Model):
-    user = models.OneToOneField(
-        CustomUser,
-        on_delete=models.CASCADE,
+    email = models.EmailField(
+        null=False,
+        blank=False,
+        unique=True,
     )
+
+    objects = CustomUserManager()
 
     def __str__(self):
-        return str(self.user)
-
-    def full_clean(self, exclude=None, validate_unique=True, validate_constraints=True):
-        return super().full_clean()
+        return self.username
