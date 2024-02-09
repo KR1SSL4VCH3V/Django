430261b88d1e6b91c0d7dc62fbcde5a1e0369f8d
Kris <107553288+KR1SSL4VCH3V@users.noreply.github.com>
2024-02-09 11:08:46 +0000

    Update urls.py

--- todo_list/todo_list/tasks/urls.py
+++ todo_list/todo_list/tasks/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from task_manager.tasks.views import HomeTaskView, CreateTaskView, DeleteTaskView
+from task_manager.tasks.views import HomeTaskView, DeleteTaskView
 
 urlpatterns = (
     path('home/', HomeTaskView.as_view(), name='home'),
-    path('create/', CreateTaskView.as_view(), name='create'),
     path('delete/<int:pk>/', DeleteTaskView.as_view(), name='delete'),
 )
