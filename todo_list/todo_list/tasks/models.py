beb7ceb4a38b641ef0975914141cf61869bfd2d9
Kris <107553288+Kris-R-Slavchev@users.noreply.github.com>
2024-02-19 21:00:06 +0000

    Update models.py

--- todo_list/todo_list/tasks/models.py
+++ todo_list/todo_list/tasks/models.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+import datetime
 from django.db import models
 from django.utils.formats import date_format
 
@@ -28,12 +30,25 @@ class Task(models.Model):
         default=False,
     )
 
-    created_date = models.DateTimeField(
+    created_date = models.DateField(
         auto_now_add=True,
+    )
+
+    due_date = models.DateField(
+        default=datetime.date.today,
     )
 
     class Meta:
         ordering = ['priority']
 
     def __str__(self):
-        return f"{self.title}. Created on {date_format(self.created_date, 'SHORT_DATE_FORMAT')}"
+        return (f"{self.title}. Created on {date_format(self.created_date, 'SHORT_DATE_FORMAT')}."
+                f" Due on {self.due_date}")
+
+    def is_expired(self):
+        return self.due_date < timezone.now() - datetime.timedelta(days=13)
+
+    def save(self, *args, **kwargs):
+        if self.created_date and not self.due_date:
+            self.due_date = self.created_date + datetime.timedelta(days=14)
+        super().save(*args, **kwargs)
