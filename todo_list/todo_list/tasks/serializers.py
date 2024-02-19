d3f6b411e27f3ddf07289b71b91c652757451d66
Kris <107553288+Kris-R-Slavchev@users.noreply.github.com>
2024-02-19 21:02:33 +0000

    Update serializers.py

--- todo_list/todo_list/tasks/serializers.py
+++ todo_list/todo_list/tasks/serializers.py
@@ -4,20 +4,38 @@ from task_manager.tasks.models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    created_date = serializers.DateField(
+        format='%d %b %Y',
+        read_only=True,
+    )
+    due_date = serializers.DateField(
+        format='%d %b %Y',
+        error_messages={
+            'invalid': 'This field may not be blank.'
+        }
+    )
+
+    @staticmethod
+    def get_due_date_message(obj):
+        if obj.is_expired():
+            return f"Task '{obj.title}' is due date and will be deleted automatically!"
+
+        return ''
+
     class Meta:
         model = Task
-        fields = ['pk', 'title', 'description', 'priority', 'created_date']
+        fields = ['pk', 'title', 'description', 'priority', 'created_date', 'due_date']
 
         user = serializers.PrimaryKeyRelatedField(
             read_only=True,
         )
 
-        @staticmethod
-        def validate_name(value):
-            if Task.objects.filter(title=value).exists():
-                raise serializers.ValidationError("Task with the same name already exists!")
-            return value
+    @staticmethod
+    def validate_title(value):
+        if Task.objects.filter(title=value).exists():
+            raise serializers.ValidationError("Task with the same name already exists!")
+        return value.capitalize()
 
-        @staticmethod
-        def clean_title(value):
-            return value.capitalize()
+    def to_internal_value(self, data):
+        data['description'] = data.get('description', '').capitalize()
+        return super(TaskSerializer, self).to_internal_value(data)
