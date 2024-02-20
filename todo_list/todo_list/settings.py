1c3a8210faecc27414030614d870ee81c5c70895
Kris <107553288+Kris-R-Slavchev@users.noreply.github.com>
2024-02-20 11:51:31 +0000

    Update settings.py

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "manager",
-        "USER": "postgres-user",
-        "PASSWORD": "password",
+        "NAME": "********",
+        "USER": "********",
+        "PASSWORD": "*********",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
